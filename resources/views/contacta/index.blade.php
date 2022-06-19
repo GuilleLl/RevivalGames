784d324c95ce44f5fe34c917de3d4924229e18d8
Guille Llosa <guillermola40@educastur.es>
2022-06-19 21:31:03 +0200

    contacta ACABAD

--- resources/views/contacta/index.blade.php
+++ resources/views/contacta/index.blade.php
@@ -3,11 +3,16 @@
 @section('content')
 
 
-    <div class="flex justify-center flex-wrap p-4 mt-5">
-        @include("contacta.form")
+<div class="flex justify-center flex-wrap p-4 mt-5">
+@if(\Session::has('success'))
+    <div class="d-flex justify-content-center col-12">
+        <div class="alert alert-success ">{{ \Session::get('success') }}</div>
     </div>
-    @if(\Session::has('success'))
-    <div class="alert alert-success">{{ \Session::get('success') }}</div>
+
     {{ \Session::forget('success') }}
     @endif
+    @include("contacta.form")
+
+</div>
+
 @endsection
